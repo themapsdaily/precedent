e5d6f750ab6a887c2e97596f9a6ee0f150679194
The Maps Daily <108807789+themapsdaily@users.noreply.github.com>
2024-02-26 21:03:42 +0530

    Update page.tsx
    Changed headline

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default async function Home() {
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          Building blocks for your Next project
+          Making Data accessible for all!
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
