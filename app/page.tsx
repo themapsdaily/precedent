342488b59e3be6cf57f7ea5fefdacc11cfdae1eb
The Maps Daily <108807789+themapsdaily@users.noreply.github.com>
2024-02-26 20:58:51 +0530

    Update page.tsx
    Twitter

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default async function Home() {
           rel="noreferrer"
           className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
         >
-          <Instagram className="h-5 w-5 text-[#1d9bf0]" />
+          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
           <p className="text-sm font-semibold text-[#1d9bf0]">
             Introducing TheMapsDaily
           </p>
