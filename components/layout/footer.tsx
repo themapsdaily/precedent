4f46975a4a926e5044e027778eaf1ad659daee49
The Maps Daily <108807789+themapsdaily@users.noreply.github.com>
2024-02-26 21:09:10 +0530

    Update footer.tsx
    Footer changes

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -7,15 +7,15 @@ export default function Footer() {
         A project by{" "}
         <a
           className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-          href="https://twitter.com/steventey"
+          href="https://instagram.com/_siddh.art.h_"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Steven Tey
+          Siddharth Petare
         </a>
       </p>
       <a
-        href="https://www.buymeacoffee.com/steventey"
+        href="https://www.buymeacoffee.com/themapsdaily"
         target="_blank"
         rel="noopener noreferrer"
         className="mx-auto mt-2 flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-6 py-2 transition-all duration-75 hover:scale-105"
